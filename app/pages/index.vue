0b905d4363b7d2b78c34649a881b86540b5749f5
Liam <liam3006x@gmail.com>
2026-01-25 17:38:22 +0000

    Improve styling

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,8 +2,9 @@
 
 <template>
     <div class="h-screen flex flex-row items-center justify-center">
-        <h1 class="text-3xl">
-            <span class="text-primary font-bold">LXST</span><span class="">.digital</span>
+        <h1 class="text-3xl flex flex-row">
+            <span class="text-primary font-bold">LXST</span>
+            <span>.digital</span>
         </h1>
     </div>
 </template>

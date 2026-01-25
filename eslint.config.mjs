c194e595153ac678218b8675151f7a0ec37c418d
Liam <liam3006x@gmail.com>
2026-01-25 17:38:10 +0000

    Fix linting etc.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,24 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default withNuxt(
-    // This helper adds the prettier plugin and displays
-    // formatting issues as ESLint errors
+    {
+        files: ['app/**/*.vue'],
+        languageOptions: {
+            parser: (await import('vue-eslint-parser')).default,
+            parserOptions: {
+                parser: (await import('@typescript-eslint/parser')).default,
+                sourceType: 'module'
+            }
+        }
+    },
+    {
+        files: ['app/**/*.ts'],
+        languageOptions: {
+            parser: (await import('@typescript-eslint/parser')).default,
+            parserOptions: {
+                sourceType: 'module'
+            }
+        }
+    },
     eslintPluginPrettierRecommended
 )

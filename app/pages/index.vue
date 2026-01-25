3b78b169a7e28f158eff8e2f5f3f3c3dd6b66696
Liam <liam3006x@gmail.com>
2026-01-25 17:17:10 +0000

    Base theming setup

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,7 +2,9 @@
 
 <template>
     <div class="h-screen flex flex-row items-center justify-center">
-        <h1 class="text-3xl"><span class="text-primary">LXST</span>.digital</h1>
+        <h1 class="text-3xl">
+            <span class="text-primary font-bold">LXST</span><span class="">.digital</span>
+        </h1>
     </div>
 </template>
 

ced7a3ccde5ea17157bdb2913f1304f54807c3e4
Liam <liam3006x@gmail.com>
2026-02-07 13:26:21 +0000

    Add listener for iOS touch improvements

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,4 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+onMounted(() => {
+    // Adding an empty listener to the body enables :active styles in iOS Safari
+    document.body.addEventListener('touchstart', () => {}, { passive: true });
+});
+onBeforeUnmount(() => {
+    document.body.removeEventListener('touchstart', () => {});
+});
+</script>
 
 <template>
     <div class="default-layout min-h-screen container mx-auto flex flex-col">
